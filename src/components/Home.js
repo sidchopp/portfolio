5b6a3763ec766174c121d6d5254a8c3d7af29248
Sid <sidchopp@gmail.com>
2021-03-09 19:14:02 -0700

    Navbar added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,16 @@
 import React, { useEffect } from "react";
+import Navbar from "./Navbar";
+import HomeContent from "./HomeContent";
+import Footer from "./Footer";
 
 function Home() {
-  return <>HI</>;
+  return (<div>
+
+    <Navbar />
+    <HomeContent />
+    <Footer />
+  </div>
+  );
 }
 
 export default Home;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,40 @@
-import React, { useEffect } from "react";
+import React, { Component } from "react";
+import { Menu, Segment } from "semantic-ui-react";
+import "semantic-ui-css/semantic.min.css";
+export default class MenuExampleInvertedSegment extends Component {
+  state = { activeItem: "home" };
 
-function Navbar() {
-  return <></>;
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+
+  render() {
+    const { activeItem } = this.state;
+
+    return (
+      <Segment inverted>
+        <Menu inverted secondary stackable>
+            <Menu.Item
+            name="Siddharth"
+            active={activeItem === "Siddharth"}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item
+            position="right"
+            name="home"
+            active={activeItem === "home"}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item
+            name="messages"
+            active={activeItem === "messages"}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item
+            name="friends"
+            active={activeItem === "friends"}
+            onClick={this.handleItemClick}
+          />
+        </Menu>
+      </Segment>
+    );
+  }
 }
-
-export default Navbar;
