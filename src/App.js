8586ed742df0da378cc00c7642c9216377c6d70f
Sid <sidchopp@gmail.com>
2021-03-10 15:09:26 -0700

    Home.js deleted

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from 'react'
-import Home from './components/Home'
+//import Home from './components/Home'
+import Navbar from "./components/Navbar";
+import HomeContent from "./components/HomeContent";
+import Footer from "./components/Footer";
 //import { BrowserRouter, Switch, Route } from "react-router-dom";
 import './stylesheets/App.css'
 
 function App() {
   return (
-    <div className="page-container">
-      <Home />
+
+      <div className="page-container"> 
+
+      <div className=" content-wrap">
+        <Navbar />
+        <HomeContent />
+       </div>
+      <div className="footer">
+        <Footer />
+      </div>
     </div>
+
+
+
   );
 }
 
