0ffcbc726660fab5165200a0cdbab85e8e762e39
Sid <sidchopp@gmail.com>
2021-08-05 15:12:59 -0600

    changes in About page

--- src/components/About.js
+++ src/components/About.js
@@ -20,15 +20,25 @@ function About() {
       >
         <Header as='h3' color='orange'>My Life so far...</Header>
       </Divider>
-      <Segment style={{ padding: '8em 0em' }}vertical >
+      <Segment style={{ padding: '8em 0em' }} vertical >
+        <Container text>
+          <Header as='h3' style={{ fontSize: '2em', color: "white" }}>
+            Career Pivot...
+          </Header>
+          <p style={{ fontSize: '1.33em', color: "white" }}>
+            From a Physics Instructor for the last 15 odd years to a recent and budding Full Stack Developer has been an amazing transformation. Fortunatley, for me I never hit a plateau in my teaching career, Physics is in my DNA now, i guess, but it's the <i>curosity to explore</i> that made me to take this challenge. Teaching to some of the best minds in the world and enjoying what you do on top of that was a blessing but I always looked for ways to <i>create</i> new things and Stack Development  gives me the joy to create something new and meaningful everyday now. It's AWESOME!!!
+
+          </p>
+          <Divider horizontal ><Icon inverted color='black' name='caret down' /></Divider>
+        </Container>
         <Container text>
           <Header as='h3' style={{ fontSize: '2em', color: "white" }}>
             Early eighties, A little later after the first Sony's walkman was sold...
-        </Header>
+          </Header>
           <p style={{ fontSize: '1.33em', color: "white" }}>
             My Father was enjoying snow in a small hill station close to our home when the doctors were cutting the umbilical chord, so that explains where i got my sense of adventurism from
-           
-        </p>
+
+          </p>
           <Divider horizontal ><Icon inverted color='black' name='caret down' /></Divider>
         </Container>
 
@@ -37,71 +47,71 @@ function About() {
         <Container text>
           <Header as='h3' style={{ fontSize: '2em', color: "white" }}>
             Mid eighties, A curious n
-        </Header>
+          </Header>
           <p style={{ fontSize: '1.33em', color: "white" }}>
             My Father was enjoying snow in a small hill station close to our home when the doctors were cutting the umbilical chord, so that explains where i got my sense of adventurism from
-           
-        </p>
+
+          </p>
           <Divider horizontal ><Icon inverted color='black' name='caret down' /></Divider>
         </Container>
         <Container text>
           <Header as='h3' style={{ fontSize: '2em', color: "white" }}>
             Early nineties, A curious n
-        </Header>
+          </Header>
           <p style={{ fontSize: '1.33em', color: "white" }}>
             My Father was enjoying snow in a small hill station close to our home when the doctors were cutting the umbilical chord, so that explains where i got my sense of adventurism from
-               
-        </p>
+
+          </p>
           <Divider horizontal ><Icon inverted color='black' name='caret down' /></Divider>
         </Container>
         <Container text>
           <Header as='h3' style={{ fontSize: '2em', color: "white" }}>
             Mid nineties, A curious n
-        </Header>
+          </Header>
           <p style={{ fontSize: '1.33em', color: "white" }}>
             My Father was enjoying snow in a small hill station close to our home when the doctors were cutting the umbilical chord, so that explains where i got my sense of adventurism from
-           
-        </p>
+
+          </p>
           <Divider horizontal ><Icon inverted color='black' name='caret down' /></Divider>
         </Container>
         <Container text>
           <Header as='h3' style={{ fontSize: '2em', color: "white" }}>
             Mid eighties, A curious n
-        </Header>
+          </Header>
           <p style={{ fontSize: '1.33em', color: "white" }}>
             My Father was enjoying snow in a small hill station close to our home when the doctors were cutting the umbilical chord, so that explains where i got my sense of adventurism from
-           
-        </p>
+
+          </p>
           <Divider horizontal ><Icon inverted color='black' name='caret down' /></Divider>
         </Container>
         <Container text>
           <Header as='h3' style={{ fontSize: '2em', color: "white" }}>
             End of Century, A curious n
-        </Header>
+          </Header>
           <p style={{ fontSize: '1.33em', color: "white" }}>
             My Father was enjoying snow in a small hill station close to our home when the doctors were cutting the umbilical chord, so that explains where i got my sense of adventurism from
-           
-        </p>
+
+          </p>
           <Divider horizontal ><Icon inverted color='black' name='caret down' /></Divider>
         </Container>
         <Container text>
           <Header as='h3' style={{ fontSize: '2em', color: "white" }}>
             Mid eighties, A curious n
-        </Header>
+          </Header>
           <p style={{ fontSize: '1.33em', color: "white" }}>
             My Father was enjoying snow in a small hill station close to our home when the doctors were cutting the umbilical chord, so that explains where i got my sense of adventurism from
-           
-        </p>
+
+          </p>
           <Divider horizontal ><Icon inverted color='black' name='caret down' /></Divider>
         </Container>
         <Container text>
           <Header as='h3' style={{ fontSize: '2em', color: "white" }}>
             Begining of a new century
-        </Header>
+          </Header>
           <p style={{ fontSize: '1.33em', color: "white" }}>
             My Father was enjoying snow in a small hill station close to our home when the doctors were cutting the umbilical chord, so that explains where i got my sense of adventurism from
-           
-        </p>
+
+          </p>
           <Divider horizontal ><Icon inverted color='black' name='caret down' /></Divider>
         </Container>
       </Segment>
