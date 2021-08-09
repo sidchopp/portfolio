960d30101ebbc24697a9d8b359e5a0b8b629fd6e
Sid <sidchopp@gmail.com>
2021-08-08 19:30:40 -0600

    conatiner added in Resume page

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,11 +1,46 @@
 import React from "react";
 import Page from "./Page";
-import { Header } from 'semantic-ui-react';
+import { Header, Container, Divider } from 'semantic-ui-react';
 
 function Resume() {
   return (
     <Page title="Resume">
-      <Header style={{ color: 'white', fontSize: '1.5em' }}>My Resume</Header>My Resume
+      <div style={{ color: 'white' }}>
+        <Container textAlign='center'>Siddharth Chopra</Container>
+        <Container textAlign='justified'>
+          <Divider />
+          <p>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
+            ligula eget dolor. Aenean massa strong. Cum sociis natoque penatibus et
+            magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis,
+            ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa
+            quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget,
+            arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.
+            Nullam dictum felis eu pede link mollis pretium. Integer tincidunt. Cras
+            dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend
+            tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac,
+            enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.
+            Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean
+            imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper
+            ultricies nisi.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
+            ligula eget dolor. Aenean massa strong. Cum sociis natoque penatibus et
+            magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis,
+            ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa
+            quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget,
+            arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.
+            Nullam dictum felis eu pede link mollis pretium. Integer tincidunt. Cras
+            dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend
+            tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac,
+            enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.
+            Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean
+            imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper
+            ultricies nisi.
+          </p>
+        </Container>
+      </div>
     </Page>
 
   )
