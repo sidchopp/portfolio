19bfea666bece5aeed23b11e1342efc8f022963a
Sid <sidchopp@gmail.com>
2021-08-08 19:02:22 -0600

    Resume.js page included

---
+++ src/components/Resume.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Page from "./Page";
+import { Header } from 'semantic-ui-react';
+
+function Resume() {
+  return (
+    <Page title="Resume">
+      <Header>My Resume</Header>My Resume
+    </Page>
+
+  )
+}
+
+export default Resume;
