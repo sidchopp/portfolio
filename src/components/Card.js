946cabde9d66db4ead2a3146b6a809ee573e79fb
Sid <sidchopp@gmail.com>
2021-08-15 16:02:25 -0600

    social icons removed from card

--- src/components/Card.js
+++ src/components/Card.js
@@ -12,16 +12,7 @@ const CardExampleImageCard = () => (
         I live in Calgary, Canada.
       </Card.Description>
     </Card.Content>
-    <Card.Content extra>
-      {/* <a>
-        <Icon name='user' />
-        10 Friends
-      </a> */}
-      <Button circular color='black' icon='github' bordered as='a' href="https://github.com/sidchopp" target="_blank" />
-      <Button circular color='black' icon='linkedin' bordered as='a' href="https://www.linkedin.com/in/sidchopp/" target="_blank" />
-      <Button circular color='black' icon='facebook' bordered as='a' href="https://www.facebook.com/siddchopp/" target="_blank" />
-      <Button circular color='black' icon='twitter' bordered as='a' href="#" target="_blank" />
-    </Card.Content>
+
   </Card>
 )
 
